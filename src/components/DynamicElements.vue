1c093b23fe70f0dfac51479b848be87cd6ae471c
shoaib uddin <shoaibuddin12fx@gmail.com>
2022-03-19 20:22:52 +0500

    accordion

--- src/components/DynamicElements.vue
+++ src/components/DynamicElements.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="accordion" id="accordionExample">
+    <!-- <div class="accordion" id="accordionExample">
       <div class="card" v-for="obj of json" :key="obj.id">
         <div class="card-header" id="headingOne">
           <h2 class="mb-0">
@@ -62,15 +62,101 @@
           </div>
         </div>
       </div>
+    </div> -->
+    <div class="accordion" role="tablist" v-for="obj of json" :key="obj.id">
+      <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block v-b-toggle="'accordion-' + obj.id" variant="info">{{
+            obj.label
+          }}</b-button>
+        </b-card-header>
+        <b-collapse
+          :id="'accordion-' + obj.id"
+          visible
+          accordion="my-accordion"
+          role="tabpanel"
+        >
+          <b-card-body>
+            <form>
+              <div v-for="element of obj.elements" :key="element.index">
+                <div v-if="element.heading" class="headings">
+                  {{ element.label }}
+                </div>
+
+                <div v-if="!element.heading" class="element">
+                  <div class="form-group row">
+                    <label class="col-sm-6 col-form-label">{{
+                      element.label
+                    }}</label>
+                    <div class="col-sm-6">
+                      <select
+                        class="form-control"
+                        id="positon"
+                        v-if="element.type == 'select'"
+                        v-on:change="passOptions($event, element)"
+                      >
+                        <option v-for="opt of element.options" :key="opt.index">
+                          {{ opt }}
+                        </option>
+                      </select>
+                      <input
+                        v-on:change="passOptions($event, element)"
+                        v-if="element.type == 'color'"
+                        type="color"
+                        class="form-control"
+                      />
+                      <input
+                        v-if="element.type == 'text'"
+                        type="text"
+                        class="form-control"
+                      />
+                      <input
+                        v-if="element.type == 'range'"
+                        type="range"
+                        class="form-control"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+            <b-card-text>{{ text }}</b-card-text>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
+
+      <!-- <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block v-b-toggle.accordion-2 variant="info"
+            >Accordion 2</b-button
+          >
+        </b-card-header>
+        <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+          <b-card-body>
+            <b-card-text>{{ text }}</b-card-text>
+          </b-card-body>
+        </b-collapse>
+      </b-card> -->
+
+      <!-- <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+          <b-button block v-b-toggle.accordion-3 variant="info"
+            >Accordion 3</b-button
+          >
+        </b-card-header>
+        <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+          <b-card-body>
+            <b-card-text>{{ text }}</b-card-text>
+          </b-card-body>
+        </b-collapse>
+      </b-card> -->
     </div>
   </div>
 </template>
+
 <script>
 const json = require("./../assets/elements.json");
-import { bus } from '../main';
-
-
-
+import { bus } from "../main";
 
 export default {
   name: "DynamicElements",
@@ -80,25 +166,25 @@ export default {
       options: {},
     };
   },
-  methods:{
-    passOptions($event, element){
+  methods: {
+    passOptions($event, element) {
       console.log(element, $event.target.value);
 
-      var obj = { ...element }
-      obj['selected_value'] =  $event.target.value;
-    //   console.log(this.options.chart);
-      bus.$emit("send dynamic options",obj);
+      var obj = { ...element };
+      obj["selected_value"] = $event.target.value;
+      //   console.log(this.options.chart);
+      bus.$emit("send dynamic options", obj);
     },
-    setOptionValues(){
+    setOptionValues() {
       this.options = {
-       "chart": this.json.filter((value)=>value.id == 2),
-      }
-      console.log("opt",this.options.chart);
-    }
+        chart: this.json.filter((value) => value.id == 2),
+      };
+      console.log("opt", this.options.chart);
+    },
   },
-  mounted(){
-    this.setOptionValues()
-  }
+  mounted() {
+    this.setOptionValues();
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue } from 'bootstrap-vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { BootstrapVue } from "bootstrap-vue";
 // import { IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
-import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
+import "bootstrap-vue/dist/bootstrap-vue-icons.min.css";
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false;
 
-import mitt from 'mitt'
-const emitter = mitt()
+// import mitt from "mitt";
+// const emitter = mitt();
 
 export const bus = new Vue();
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
